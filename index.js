2703c919da4f4964cfbc10be8b8feea9489fc1ac
George-Okumu <gokumu12@gmail.com>
2022-02-28 08:35:13 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation) {
+    let headquater = 42;
+    if (pickupLocation >= headquater) {
+        let numberOfBlocksFromHeadquater = pickupLocation - headquater;
+        return numberOfBlocksFromHeadquater;
+    } else {
+        return headquater - pickupLocation;
+    }
+}
+
+function distanceFromHqInFeet(distance) {
+    let distanceFromHqInFeet = distanceFromHqInBlocks(distance) * 264;
+
+    return distanceFromHqInFeet;
+}
+
+function distanceTravelledInFeet(start, end) {
+    if (start > end) {
+        return (start - end) * 264;
+    } else {
+        return (end - start) * 264;
+    }
+
+}
+
+function calculatesFarePrice(start, destination) {
+    let distanceToGo = distanceTravelledInFeet(start, destination);
+    if (distanceToGo <= 400) {
+        return 0;
+    } else if (distanceToGo > 400 && distanceToGo <= 2000) {
+        return .02 * (distanceToGo - 400);
+    } else if (distanceToGo > 2000 && distanceToGo < 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
